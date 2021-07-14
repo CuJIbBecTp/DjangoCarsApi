699b75d0767b2e0c7681beddb5c10f6c9b4c27d7
CuJIbBecTp <fiodar.hancharou@inria.fr>
2021-07-14 14:30:03 +0200

    working ModelSerializer

--- project/Cars_Api/serializers.py
+++ project/Cars_Api/serializers.py
@@ -2,19 +2,7 @@ from rest_framework import serializers
 from .models import CarModel
 
 
-class CarSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    make = serializers.CharField(max_length=100)
-    model = serializers.CharField(max_length=100)
-    rate = serializers.FloatField()
-
-    def create(self, validated_data):
-        return CarModel.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.id = validated_data.get('id', instance.id)
-        instance.make = validated_data.get('make', instance.make)
-        instance.model = validated_data.get('model', instance.model)
-        instance.rate = validated_data.get('rate', instance.rate)
-        instance.save()
-        return instance
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarModel
+        fields = ['id', 'make', 'model', 'rate']
