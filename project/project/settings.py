e56b2f948f8438d0c55cf7fecb29a8b27958692e
CuJIbBecTp <fiodar.hancharou@inria.fr>
2021-07-16 13:16:19 +0200

    Heroku settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,4 +115,4 @@ USE_L10N = True
 
 USE_TZ = True
 
-DISABLE_COLLECTSTATIC=1
+django_heroku.settings(locals())
